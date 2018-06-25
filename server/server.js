c2321f8b12f9b32dbab6952a6915c9b5dd254325
kmirijan <34608342+kmirijan@users.noreply.github.com>
2018-06-25 16:59:11 -0700

    emit all message

--- server/server.js
+++ server/server.js
@@ -20,8 +20,13 @@ io.on('connection', (socket) => {
     createAt: 123
   })
 
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
 
   socket.on('disconnect', () => {
