1b672cf9f9ce79e05fd213d0c548dbb8f2b766a3
kmirijan <34608342+kmirijan@users.noreply.github.com>
2018-06-25 17:08:18 -0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,14 +15,25 @@ io.on('connection', (socket) => {
   console.log('new user connected')
 
   socket.emit('newMessage', {
-    from: 'person',
-    text: 'This is a new message',
-    createAt: 123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()
   })
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
